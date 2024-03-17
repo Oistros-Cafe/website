84d91a07fc52bef4ef7a84d057b8f35ee68c9325
Antonis Anagnostou <anagnostou.antonis@gmail.com>
2024-03-17 12:14:55 +0000

    Fix configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: "/website",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
