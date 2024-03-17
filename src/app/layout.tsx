e2de984962012eaf021b9177681dd603aa43ae6f
Antonis Anagnostou <anagnostou.antonis@gmail.com>
2024-03-17 18:07:35 +0000

    Changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,10 +23,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+        <link rel="shortcut icon" href="/image/webico.png" type="image/png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Syne&display=swap" rel="stylesheet" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://oistroscafe.gr/" />
+        <meta property="og:title" content="Oistros Cafe | SHop"/>
+        <meta property="og:description" content="Ανακαλύψτε τον καφέ που σας ταιριάζει"/>
+        <meta property="og:image" content="https://oistroscafe.gr/image/oistros-2.JPG"/>
+        <meta property="og:image:width" content="1024"/>
+        <meta property="og:image:height" content="684"/>
       </head>
       <body className={roboto.className}>
         <Layout>
